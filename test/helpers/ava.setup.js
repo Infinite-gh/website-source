9857b853ef10daf5818a63bfab6e71e2f786e8dd
KZacharski <kacperzacharski@icloud.com>
2022-02-04 09:43:47 +0100

    Nuxt creators messed up the tests.

--- test/helpers/ava.setup.js
+++ test/helpers/ava.setup.js
@@ -1,14 +1,22 @@
-if (process.env.TEST === 'unit') {
+const hooks = require('require-extension-hooks')
+
+if (process.env.TEST === 'unit') { // These checks are missing for create-nuxt-app generated files
   require('jsdom-global')()
+  require('browser-env')
+  const Vue = require('vue')
+  Vue.config.productionTip = false
   // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
   window.Date = global.Date = Date
 }
 
-const Vue = require('vue')
-Vue.config.productionTip = false
+if (process.env.TEST === 'e2e') {  // These checks are missing for create-nuxt-app generated files
+  const Vue = require('vue')
+  Vue.config.productionTip = false
+}
 
-const hooks = require('require-extension-hooks')
-hooks('vue').plugin('vue').push()
+hooks('vue')
+  .plugin('vue')
+  .push()
 hooks(['vue', 'js'])
   .exclude(({ filename }) => filename.match(/\/node_modules\//))
   .plugin('babel')
